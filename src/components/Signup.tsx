5353ca8021219d0ba2f1c7980653f28d230384a5
anksthakur <anksthakur7@gmail.com>
2024-10-21 17:34:45 +0530

    form validation

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,59 +3,102 @@ import { useState } from "react";
 
 
 export default function Signup() {
-    const [email, setEmail] = useState<any>('');
-    const [password, setPassword] = useState<any>('');
-    const [name,setName] = useState<any>('')
+    const [formData, setFormdata] = useState<any>({ name: '', email: '', password: '', confirmPassword: '' });
+    const [error, setError] = useState<any>({});
+
     const router = useNavigate();
 
-    const adminEmail = "anup.kumar@contriverz.com"
-    const adminPassword = "Admin@123"
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormdata({ ...formData, [name]: value });
+    }
+    const validate: any = () => {
+        const newError:any = {};
+        const { name, email, password, confirmPassword } = formData;
+        if (!name) {
+            newError.name = "Name is required";
+        }
+        if (!email) {
+            newError.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            newError.email = "email is invalid"
+        }
+        if (!password) {
+            newError.password = 'Password is required';
+        } else if (password.length < 6) {
+            newError.password = "password must be at least 6 characters"
+        }
+        if (password !== confirmPassword) {
+            newError.confirmPassword = "password do not matched"
+        }
+        setError(newError);
+        return Object.keys(newError).length === 0
+    }
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        if (email === adminEmail && password === adminPassword) {
+        if (validate()) {
+            console.log("form data :", formData)
             router('/signin');
-        } else {
-            alert("check your credential");
         }
+        
     }
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-sm">
                 <h1 className="font-bold text-2xl text-center mb-6">Sign Up</h1>
                 <form onSubmit={handleSubmit}>
-                <div className="mb-4">
+                    <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-700">Name</label>
                         <input
                             className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                             type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
                             placeholder="Enter your Name"
                             required
                         />
+                        {error.name && <p className="text-red-500">{error.name}</p>}
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-700">Email Address</label>
                         <input
                             className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                             type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
                             placeholder="Enter your email address"
                             required
                         />
+                         {error.email && <p className="text-red-500">{error.email}</p>}
                     </div>
                     <div className="mb-6">
                         <label className="block text-sm font-medium text-gray-700">Password</label>
                         <input
                             className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                             type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
                             placeholder="Enter your password"
                             required
                         />
+                         {error.password && <p className="text-red-500">{error.password}</p>}
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-sm font-medium text-gray-700">Confirm Password</label>
+                        <input
+                            className="mt-1 block w-full border border-gray-300 rounded-md p-2"
+                            type="password"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            placeholder="Enter your password"
+                            required
+                        />
+                         {error.confirmPassword && <p className="text-red-500">{error.confirmPassword}</p>}
                     </div>
                     <div className="mb-4 ">
                         <button className="w-full bg-green-400 text-white rounded-md py-2 hover:bg-blue-500 ">
@@ -70,7 +113,7 @@ export default function Signup() {
                             </NavLink>
                         </p>
                     </div>
-                    
+
                 </form>
             </div>
         </div>
